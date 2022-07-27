c3d6d9264b7b69d4891a6c92df640166ed26cb66
Moe <m.ibrahim0228@gmail.com>
2022-07-27 14:25:01 -0400

    formatted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,16 +7,12 @@ public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Executor executor = Executors.newSingleThreadExecutor();
         List<Future> futureList = new ArrayList<>();
-        for(int i=0; i<6; i++){
-            Callable<Integer> integerCallable= new Callable<Integer>() {
-                @Override
-                public Integer call() throws Exception {
-                    Random random = new Random();
-                    Integer num = random.nextInt();
-                    return num;
-                }
-
-            } ;
+        for (int i = 0; i < 6; i++) {
+            Callable<Integer> integerCallable = () -> {
+                Random random = new Random();
+                Integer num = random.nextInt();
+                return num;
+            };
             futureList.add(((ExecutorService) executor).submit(integerCallable));
 //
 
@@ -25,7 +21,7 @@ public class Main {
         futureList.get(4).cancel(true);
 
 
-        for(Future future: futureList) {
+        for (Future future : futureList) {
             if (!future.isCancelled()) {
                 future.get();
             }
@@ -33,6 +29,7 @@ public class Main {
         System.out.println(futureList);
         System.out.println(countFinishedFutures(futureList));
     }
+
     public static int countFinishedFutures(List<Future> futures) {
         return (int) futures.stream().filter(obj -> !obj.isCancelled()).filter(nonCancelled -> nonCancelled.isDone()).count();
     }
